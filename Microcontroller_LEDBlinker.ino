72b10cedb8280ad5893a783bff880ff8a82fa00e
Awaaz Dahal <65304348+awd30@users.noreply.github.com>
2022-10-12 20:19:28 -0400

    Assignment 8 - LEDBlinker arduino code

---
+++ Microcontroller_LEDBlinker.ino
@@ -0,0 +1,23 @@
+//Awaaz Dahal
+//Single LED
+const int BUTTON_PIN = 11;  
+const int LED_PIN =  12;
+int buttonState = 0;   
+void setup()
+{ 
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+void loop()
+{
+  buttonState = digitalRead(BUTTON_PIN);
+  if(buttonState == LOW){        // If button is pressed
+    digitalWrite(LED_PIN, HIGH); // turn on LED
+	delay(1000);//pause
+  	digitalWrite(LED_PIN, LOW);  //turn off led
+    delay(1000);
+  }
+  else                           
+    digitalWrite(LED_PIN, LOW);  // turn off LED   
+  } 
