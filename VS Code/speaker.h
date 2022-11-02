9187126450af280e8334455ae42bf2d0037cc48e
lukespowerconverters <rachsolak@gmail.com>
2022-11-02 10:27:46 -0400

    added new files for speaker class + functions

---
+++ VS Code/speaker.h
@@ -0,0 +1,9 @@
+#ifndef SPEAKER_H
+#define SPEAKER_H
+
+#include <..\Arduino\libraries\Talkie\src\Talkie.h>
+#include <Talkie\src\Vocab_US_TI99.h>
+#include <Talkie\src\Vocab_Special.h>
+#include <Talkie\src\Vocab_US_Acorn.h>
+
+#endif
